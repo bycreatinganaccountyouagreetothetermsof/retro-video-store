309287da86caeee11c4459c0e3c9c8983f7cede4
lain <bycreatinganaccountyouagreetothetermsof@users.noreply.github.com>
2021-11-08 18:19:00 -0800

    to_dict for customer and video models

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,5 @@
 from app import db
+from sorcery import dict_of
 
 
 class Customer(db.Model):
@@ -7,3 +8,14 @@ class Customer(db.Model):
     postal_code = db.Column(db.Integer)
     phone_number = db.Column(db.String)
     registered_at = db.Column(db.DateTime)
+
+    def to_dict(self):
+        return dict_of(
+            [
+                self.id,
+                self.name,
+                self.postal_code,
+                self.phone_number,
+                self.registered_at,
+            ]
+        )
--- app/models/video.py
+++ app/models/video.py
@@ -1,4 +1,5 @@
 from app import db
+from sorcery import dict_of
 
 
 class Video(db.Model):
@@ -6,3 +7,13 @@ class Video(db.Model):
     title = db.Column(db.String)
     release_date = db.Column(db.DateTime)
     total_inventory = db.Column(db.Integer)
+
+    def to_dict(self):
+        return dict_of(
+            [
+                self.id,
+                self.title,
+                self.release_date,
+                self.total_inventory,
+            ]
+        )
