ad947b7f511997d818f72351ea9dca76c8c51e8f
lain <bycreatinganaccountyouagreetothetermsof@users.noreply.github.com>
2021-11-08 17:51:07 -0800

    basic customer and video models

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,9 @@
 from app import db
 
+
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    postal_code = db.Column(db.Integer)
+    phone_number = db.Column(db.String)
+    registered_at = db.Column(db.DateTime)
